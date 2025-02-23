a362efa1f857470bd33ebbb820bfff7cb50cf74d
Matteo Badini <matteo.badini@gellify.com>
2025-02-23 13:57:28 +0100

    refactor: cleanup dead code

--- src/app/[locale]/(app)/(pillars)/banking/page.tsx
+++ src/app/[locale]/(app)/(pillars)/banking/page.tsx
@@ -1,10 +1,8 @@
-import BankingOverview from "./_components/banking-overview";
-
 export default function BankingPage() {
   return (
     <>
       <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-        <BankingOverview />
+        TODO
         {/* <BankAccountSelector /> */}
       </div>
     </>
