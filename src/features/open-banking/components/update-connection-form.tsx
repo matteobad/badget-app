1480013e328f3a3110dd4dd02d443bfb949a45e3
Matteo Badini <matteo.badini@gellify.com>
2025-02-23 20:13:01 +0100

    style: update connections

--- src/features/open-banking/components/update-connection-form.tsx
+++ src/features/open-banking/components/update-connection-form.tsx
@@ -2,6 +2,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import {
   ArrowUpRightIcon,
   CreditCardIcon,
+  EllipsisIcon,
   Loader2Icon,
   QrCodeIcon,
   WalletIcon,
@@ -11,14 +12,17 @@ import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 import { type z } from "zod";
 
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "~/components/ui/accordion";
 import { Button } from "~/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
 import { Form } from "~/components/ui/form";
+import { Switch } from "~/components/ui/switch";
 import { cn } from "~/lib/utils";
 import { updateConnectedAccountAction } from "../server/actions";
 import { type getConnectionsWithAccountsForUser } from "../server/queries";
@@ -67,70 +71,74 @@ export default function UpdateConnectionForm({
           <code>{JSON.stringify(form.formState.errors, null, 2)}</code>
         </pre> */}
 
-        <Accordion type="multiple">
-          <AccordionItem value="attchament">
-            <AccordionTrigger className="text-sm">
-              Conti collegati
-            </AccordionTrigger>
-            <AccordionContent className="space-y-2">
-              <ul className="mt-4 space-y-4">
-                {connection.accounts.map((account) => (
-                  <li
-                    key={account.id}
-                    className={cn(
-                      "group flex items-center justify-between",
-                      "rounded-lg p-2",
-                      "hover:bg-zinc-100 dark:hover:bg-zinc-800/50",
-                      "transition-all duration-200",
-                    )}
-                  >
-                    <div className="flex items-center gap-2">
-                      <div
-                        className={cn("rounded-lg p-1.5", {
-                          "bg-emerald-100 dark:bg-emerald-900/30":
-                            account.type === "savings",
-                          "bg-blue-100 dark:bg-blue-900/30":
-                            account.type === "checking",
-                          "bg-purple-100 dark:bg-purple-900/30":
-                            account.type === "investment",
-                        })}
-                      >
-                        {account.type === "savings" && (
-                          <WalletIcon className="h-3.5 w-3.5 text-emerald-600 dark:text-emerald-400" />
-                        )}
-                        {account.type === "checking" && (
-                          <QrCodeIcon className="h-3.5 w-3.5 text-blue-600 dark:text-blue-400" />
-                        )}
-                        {account.type === "investment" && (
-                          <ArrowUpRightIcon className="h-3.5 w-3.5 text-purple-600 dark:text-purple-400" />
-                        )}
-                        {account.type === "debt" && (
-                          <CreditCardIcon className="h-3.5 w-3.5 text-red-600 dark:text-red-400" />
-                        )}
-                      </div>
-                      <div>
-                        <h3 className="text-xs font-medium text-zinc-900 dark:text-zinc-100">
-                          {account.name}
-                        </h3>
-                        {account.description && (
-                          <p className="text-[11px] text-zinc-600 dark:text-zinc-400">
-                            {account.description}
-                          </p>
-                        )}
-                      </div>
-                    </div>
+        <ul className="space-y-4">
+          {connection.accounts.map((account) => (
+            <li
+              key={account.id}
+              className={cn(
+                "group flex items-center justify-between",
+                "py-1",
+                "transition-all duration-200",
+              )}
+            >
+              <div className="flex items-center gap-2">
+                <div
+                  className={cn("rounded-lg p-1.5", {
+                    "bg-emerald-100 dark:bg-emerald-900/30":
+                      account.type === "savings",
+                    "bg-blue-100 dark:bg-blue-900/30":
+                      account.type === "checking",
+                    "bg-purple-100 dark:bg-purple-900/30":
+                      account.type === "investment",
+                  })}
+                >
+                  {account.type === "savings" && (
+                    <WalletIcon className="h-3.5 w-3.5 text-emerald-600 dark:text-emerald-400" />
+                  )}
+                  {account.type === "checking" && (
+                    <QrCodeIcon className="h-3.5 w-3.5 text-blue-600 dark:text-blue-400" />
+                  )}
+                  {account.type === "investment" && (
+                    <ArrowUpRightIcon className="h-3.5 w-3.5 text-purple-600 dark:text-purple-400" />
+                  )}
+                  {account.type === "debt" && (
+                    <CreditCardIcon className="h-3.5 w-3.5 text-red-600 dark:text-red-400" />
+                  )}
+                </div>
+                <div>
+                  <h3 className="font-medium">{account.name}</h3>
+                  {account.description && (
+                    <p className="text-zinc-600 dark:text-zinc-400">
+                      {account.description}
+                    </p>
+                  )}
+                </div>
+              </div>
 
-                    <div className="text-right">
-                      <span className="text-xs font-medium text-zinc-900 dark:text-zinc-100">
-                        {account.balance}
-                      </span>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
+              <div className="flex items-center gap-4 text-right">
+                <span className="font-medium text-zinc-900 dark:text-zinc-100">
+                  {account.balance}
+                </span>
+
+                <Switch />
+
+                <DropdownMenu>
+                  <DropdownMenuTrigger>
+                    <EllipsisIcon className="size-4" />
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>Profile</DropdownMenuItem>
+                    <DropdownMenuItem>Billing</DropdownMenuItem>
+                    <DropdownMenuItem>Team</DropdownMenuItem>
+                    <DropdownMenuItem>Subscription</DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </div>
+            </li>
+          ))}
+        </ul>
 
         <div className="grow"></div>
         <div className="flex items-center gap-4">
