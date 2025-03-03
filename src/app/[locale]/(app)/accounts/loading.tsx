b672c13a85b87d367e91ab7588cb7762fbd70f62
Matteo Badini <matteo.badini@gellify.com>
2025-03-03 17:09:27 +0100

    style: cleanup loading states

--- src/app/[locale]/(app)/accounts/loading.tsx
+++ src/app/[locale]/(app)/accounts/loading.tsx
@@ -1,41 +1,19 @@
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbList,
-  BreadcrumbSeparator,
-} from "~/components/ui/breadcrumb";
-import { Separator } from "~/components/ui/separator";
-import { SidebarTrigger } from "~/components/ui/sidebar";
 import { Skeleton } from "~/components/ui/skeleton";
 
 export default function Loading() {
   return (
     <>
-      <header className="flex h-16 shrink-0 items-center gap-2">
-        <div className="flex items-center gap-2 px-4">
-          <SidebarTrigger className="-ml-1" />
-          <Separator orientation="vertical" className="mr-2 h-4" />
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem className="hidden md:block">
-                <Skeleton className="h-4 w-[120px]" />
-              </BreadcrumbItem>
-              <BreadcrumbSeparator className="hidden md:block" />
-              <BreadcrumbItem>
-                <Skeleton className="h-4 w-[120px]" />
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
-        </div>
+      <header className="flex h-16 shrink-0 flex-col items-start gap-2 px-4">
+        <Skeleton className="h-6 w-[220px]" />
+        <Skeleton className="h-4 w-[350px]" />
       </header>
-      <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-        <div className="grid auto-rows-min gap-4 md:grid-cols-2 lg:grid-cols-4">
+      <div className="flex flex-1 flex-col gap-4 p-4">
+        <div className="grid auto-rows-min gap-4 lg:grid-cols-2">
           <Skeleton className="aspect-video rounded-xl bg-muted/50" />
           <Skeleton className="aspect-video rounded-xl bg-muted/50" />
           <Skeleton className="aspect-video rounded-xl bg-muted/50" />
           <Skeleton className="aspect-video rounded-xl bg-muted/50" />
         </div>
-        <Skeleton className="grid min-h-[100vh] flex-1 overflow-hidden rounded-xl bg-muted/50 md:min-h-min md:grid-cols-3" />
       </div>
     </>
   );
