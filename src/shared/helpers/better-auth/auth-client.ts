56c1518963d84a0f90418036f6fef2dbfa0b5082
Matteo Badini <matteo.badini@gellify.com>
2025-07-23 11:13:21 +0200

    fix: better auth url

--- src/shared/helpers/better-auth/auth-client.ts
+++ src/shared/helpers/better-auth/auth-client.ts
@@ -3,7 +3,7 @@ import { createAuthClient } from "better-auth/react";
 
 const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: "http://localhost:3000",
+  // baseURL: "http://localhost:3000",
   plugins: [passkeyClient()],
 });
 
