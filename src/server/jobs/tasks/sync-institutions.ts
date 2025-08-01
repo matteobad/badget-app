3f41a8ffc44fe3b2b5c73d2e4241bfae2dc0fe95
Matteo Badini <matteo.badini@gellify.com>
2025-08-01 18:59:48 +0200

    perf: upsert institutions in batches

--- src/server/jobs/tasks/sync-institutions.ts
+++ src/server/jobs/tasks/sync-institutions.ts
@@ -4,6 +4,8 @@ import { db } from "~/server/db";
 import { institution_table } from "~/server/db/schema/open-banking";
 import { buildConflictUpdateColumns } from "~/server/db/utils";
 
+const BATCH_SIZE = 500;
+
 // Fan-out pattern. We want to trigger a task for each bank account (Transactions, Balance)
 export const syncInstitutions = schedules.task({
   id: "sync-institutions",
@@ -18,26 +20,31 @@ export const syncInstitutions = schedules.task({
   run: async () => {
     try {
       const provider = getBankAccountProvider("gocardless");
-      const data = await provider.getInstitutions({
+      const institutionsData = await provider.getInstitutions({
         countryCode: "IT",
       });
 
-      if (data.length === 0) {
+      if (institutionsData.length === 0) {
         logger.error("Institutions not found");
         throw new Error("Institutions not found");
       }
 
-      await db
-        .insert(institution_table)
-        .values(data)
-        .onConflictDoUpdate({
-          target: [institution_table.originalId],
-          set: buildConflictUpdateColumns(institution_table, [
-            "countries",
-            "logo",
-          ]),
-        })
-        .returning();
+      // Upsert institutions in batches of 500
+      // This is to avoid memory issues with the DB
+      for (let i = 0; i < institutionsData.length; i += BATCH_SIZE) {
+        const institutionsBatch = institutionsData.slice(i, i + BATCH_SIZE);
+        await db
+          .insert(institution_table)
+          .values(institutionsBatch)
+          .onConflictDoUpdate({
+            target: [institution_table.originalId],
+            set: buildConflictUpdateColumns(institution_table, [
+              "countries",
+              "logo",
+            ]),
+          })
+          .returning();
+      }
 
       logger.info("Synced institutions completed");
     } catch (error) {
