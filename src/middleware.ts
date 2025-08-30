dfd3671d846cb7b25fafdfd9a26a750ca6ad406a
Matteo Badini <matteo.badini@gellify.com>
2025-08-30 23:09:15 +0200

    fix: redirect loop with en locale in the url

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,30 +8,47 @@ import { defaultLocale, locales } from "./shared/locales/config";
 const I18nMiddleware = createI18nMiddleware({
   locales: locales,
   defaultLocale: defaultLocale,
-  urlMappingStrategy: "rewriteDefault",
+  urlMappingStrategy: "rewrite",
 });
 
 export async function middleware(request: NextRequest) {
+  const response = I18nMiddleware(request);
   const sessionCookie = getSessionCookie(request);
-  const { pathname } = request.nextUrl;
+  const nextUrl = request.nextUrl;
+  const pathnameLocale = nextUrl.pathname.split("/", 2)?.[1];
 
-  // api routes need to pass through middleware
-  // but if passing on i18nMiddleware we get a 404
-  if (pathname.includes("api")) {
-    return;
-  }
+  // Remove the locale from the pathname
+  const pathnameWithoutLocale = pathnameLocale
+    ? nextUrl.pathname.slice(pathnameLocale.length + 1)
+    : nextUrl.pathname;
+
+  // Create a new URL without the locale in the pathname
+  const newUrl = new URL(pathnameWithoutLocale || "/", request.url);
+
+  const encodedSearchParams = `${newUrl?.pathname?.substring(1)}${
+    newUrl.search
+  }`;
 
   // 1. Not authenticated
-  if (!sessionCookie && pathname !== "/sign-in" && pathname !== "/sign-up") {
+  if (
+    !sessionCookie &&
+    newUrl.pathname !== "/sign-in" &&
+    newUrl.pathname !== "/sign-up"
+  ) {
     const url = new URL("/sign-in", request.url);
+
+    if (encodedSearchParams) {
+      url.searchParams.append("return_to", encodedSearchParams);
+    }
+
     return NextResponse.redirect(url);
   }
 
   // 2. If authenticated, proceed with other checks
   if (sessionCookie) {
-    if (pathname !== "/spaces/create" && pathname !== "/spaces") {
+    if (newUrl.pathname !== "/spaces/create" && newUrl.pathname !== "/spaces") {
       // Check if the URL contains an invite code
-      const inviteCodeMatch = pathname.startsWith("/spaces/invite/");
+      const inviteCodeMatch = newUrl.pathname.startsWith("/spaces/invite/");
 
       if (inviteCodeMatch) {
         const url = new URL("/", request.url);
@@ -44,14 +61,10 @@ export async function middleware(request: NextRequest) {
     }
   }
 
-  return I18nMiddleware(request);
+  // If all checks pass, return the original or updated response
+  return response;
 }
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|api).*)"],
 };
