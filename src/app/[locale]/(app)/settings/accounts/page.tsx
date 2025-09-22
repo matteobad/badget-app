6ebd3d7b374dd877fe0bc598fdfa31d1503e5901
Matteo Badini <matteo.badini@gellify.com>
2025-09-22 14:30:36 +0200

    cleanup bank connections page

--- src/app/[locale]/(app)/settings/accounts/page.tsx
+++ src/app/[locale]/(app)/settings/accounts/page.tsx
@@ -3,12 +3,11 @@ import { ConnectedAccounts } from "~/components/bank-connection/connected-accoun
 import { prefetch, trpc } from "~/shared/helpers/trpc/server";
 
 export const metadata: Metadata = {
-  title: "Accounts | Badget",
+  title: "Bank connections | Badget",
 };
 
 export default async function Page() {
   prefetch(trpc.bankConnection.get.queryOptions());
-  prefetch(trpc.bankAccount.get.queryOptions({ manual: true }));
 
   return (
     <div className="space-y-12">
--- src/components/bank-connection/bank-account-list.tsx
+++ src/components/bank-connection/bank-account-list.tsx
@@ -1,11 +1,9 @@
 import { BankConnections } from "./bank-connections";
-import { ManualAccounts } from "./manual-accounts";
 
 export function BankAccountList() {
   return (
     <>
       <BankConnections />
-      <ManualAccounts />
     </>
   );
 }
